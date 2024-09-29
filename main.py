517d259f2dcdb9ce42a11fb2e7b0a3c7ed8af039
Igniter-909 <igniterofficial909505@gmail.com>
2024-09-29 14:28:43 +0530

    Data Transformation Done Successfully

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage02_data_validation import DataValidationtrainingPipeline
+from textSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 logger.propagate = True
@@ -20,6 +21,17 @@ try:
     logger.info(f">>>>>>> Stage {STAGE_NAME} strated <<<<<<<<<")
     data_validation = DataValidationtrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} strated <<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<")
 
 except Exception as e:
