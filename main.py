f46a0db24bd8c5d7485f191670452b0d20c55ea9
Igniter-909 <igniterofficial909505@gmail.com>
2024-09-30 11:26:35 +0530

    Model Training Completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage02_data_validation import DataValidationtrainingPipeline
 from textSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 logger.propagate = True
@@ -37,3 +38,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>>>> Stage {STAGE_NAME} strated <<<<<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
